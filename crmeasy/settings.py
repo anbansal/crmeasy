6f0b867aeea664588218e9082c595e0f3d846cfa
Ankur <ankurbnsl8099@gmail.com>
2019-10-30 00:39:42 -0400

    created the marketing app

--- crmeasy/settings.py
+++ crmeasy/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crmeasy.marketing',
 ]
 
 MIDDLEWARE = [
@@ -137,6 +138,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
