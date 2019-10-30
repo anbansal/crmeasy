2e8c90668712f50af12d96dbfcdc55a9cdefb9b4
Ankur <ankurbnsl8099@gmail.com>
2019-10-30 02:40:14 -0400

    created the home page template

--- crmeasy/settings.py
+++ crmeasy/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def get_env_variable(var_name):
     """ Get the environment variable or return exception """
     try:
